d31932cc7345c8de8dd8b93add02b96aef746a2c
wumiejiao <3479961284@qq.com>
2021-01-04 19:59:36 +0800

    first commit

---
+++ src/main/java/Meijiao1.java
@@ -0,0 +1,6 @@
+/**
+ * @author:Mei
+ * @create:2021-01-04 19:45
+ */
+public class Meijiao1 {
+}
---
+++ src/main/java/Meijiao2.java
@@ -0,0 +1,10 @@
+/**
+ * @author:Mei
+ * @create:2021-01-04 19:50
+ */
+public class Meijiao2 {
+
+    public static void main(String[] args) {
+        System.out.println("我要进行打印了");
+    }
+}
